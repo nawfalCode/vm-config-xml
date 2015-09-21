dab246326806d4f2d8fd4e09e29ad25e729b54ce
Nawfal Mahdi <nawfalrezo@gmail.com>
2015-09-21 13:46:48 +1000

    Done... Create a new XML with updatging the BW in and out bounds

--- app.js
+++ app.js
@@ -2,14 +2,32 @@
  * Created by Nawfal on 19-Sep-15.
  */
 
-var xml2json=require('xml2js');
-var js2xml=require('js2xmlparser');
-var fileSystem=require('fs');
-var parser=xml2json.Parser();
-fileSystem.readFile(__dirname+"/xmldata.xml",function(err,xmldata){
-    parser.parseString(xmldata,function(err,data){
-        console.dir(data);
-});
-//var parseString=xml2json.
+var fsIn = require('fs');
+var fsOut = require('fs');
+var tkn = require('tkn');
+var outStr = "";
+var newInboundValue = 500;
+var newOutboundValue = 1500;
 
+
+fsIn.readFile(__dirname + "/xmldata.xml", 'utf8', function (err, xmldata) {
+    var tokens = xmldata.split('\r');
+
+    console.dir(tokens.length);
+    tokens.forEach(function (element, index, data) {
+        if (element.indexOf("<inbound") > -1) {
+            var bwAR = tkn.tokenise(element);
+            element = element.replace(bwAR[2], newInboundValue);
+            outStr += element;
+        } else if (element.indexOf("<outbound") > -1) {
+            var bwAR = tkn.tokenise(element);
+            element = element.replace(bwAR[2], newOutboundValue);
+            outStr += element;
+        }
+        else {
+            outStr += element;
+        }
+    });
+    fsOut.writeFile(__dirname + "/xmldata2.xml", outStr);
 });
+
